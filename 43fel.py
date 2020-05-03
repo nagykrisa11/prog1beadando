eff3b042314846348097d6824beebc06fa75eb53
unknown <nagykrisa@gmail.hu>
2020-05-03 17:25:13 +0200

    43fel

---
+++ 43fel.py
@@ -0,0 +1,21 @@
+def roman_to_int(s):
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_val = 0
+    for i in range(len(s)):
+        if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+            int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+        else:
+            int_val += rom_val[s[i]]
+    return int_val
+
+rom = []
+int = []
+sum = 0
+while(sum<1000):
+    s = input()
+    rom.append(s)
+    int.append(roman_to_int(s))
+    sum += roman_to_int(s)
+
+for i in range(len(rom)):
+    print('{} -> {}'.format(rom[i], int[i]))
